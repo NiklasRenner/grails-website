c360247ae6a7315a27f8229dbf93dfc6fd390238
Niklas Renner Nielsen <NiklasRennerNielsen@gmail.com>
2017-02-20 23:31:44 +0100

    updated failing tests :)

--- src/test/groovy/dk/renner/website/RequestInfoServiceSpec.groovy
+++ src/test/groovy/dk/renner/website/RequestInfoServiceSpec.groovy
@@ -1,8 +1,10 @@
 package dk.renner.website
 
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
+@Mock(RequestInfo)
 @TestFor(RequestInfoService)
 class RequestInfoServiceSpec extends Specification {
 
--- src/test/groovy/dk/renner/website/UserControllerSpec.groovy
+++ src/test/groovy/dk/renner/website/UserControllerSpec.groovy
@@ -1,18 +1,27 @@
 package dk.renner.website
 
 import dk.renner.website.controllers.UserController
+import grails.test.mixin.Mock
 import grails.test.mixin.TestFor
 import spock.lang.Specification
 
+@Mock([RequestInfo, User])
 @TestFor(UserController)
 class UserControllerSpec extends Specification {
+
+    def setup() {
+        controller.userService = Mock(UserService)
+        controller.requestInfoService = Mock(RequestInfoService)
+    }
 
     def "test"() {
         when:
             controller.index()
 
         then:
-            true
+            1 * controller.userService.findAll()
+            1 * controller.requestInfoService.countAll()
+            1 * controller.requestInfoService.findLastRequests()
     }
 
 }
