d7bb82fa2bc508bd0fcc4d99c5b0c6dc14aed76b
Niklas Renner Nielsen <NiklasRennerNielsen@gmail.com>
2017-02-22 21:10:38 +0100

    created admin user for tests.

--- grails-app/init/dk/renner/website/BootStrap.groovy
+++ grails-app/init/dk/renner/website/BootStrap.groovy
@@ -1,8 +1,16 @@
 package dk.renner.website
 
+import grails.util.Environment
+
 class BootStrap {
 
     def init = { servletContext ->
+        if (Environment.current == Environment.TEST) {
+            User user = new User(username: 'Test', password: 'Test').save flush: true
+            Role role = new Role(authority: 'ROLE_ADMIN').save flush: true
+
+            UserRole.create user, role
+        }
     }
 
     def destroy = {
